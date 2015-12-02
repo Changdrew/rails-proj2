a2ea6a836d667bbcdd408a7ce0b72e80cb9a9ece
vagrant <vagrant@rails-dev-box>
2015-12-02 18:01:06 +0000

    fixed confclits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202105113) do
+ActiveRecord::Schema.define(version: 20151202174438) do
 
   create_table "events", force: :cascade do |t|
     t.string   "address"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20151202105113) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
