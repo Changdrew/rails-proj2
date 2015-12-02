4b9ba023b7ea25dc838cbe08fc66f33eda1a574c
Andrew <changdrew@berkeley.edu>
2015-12-02 21:59:22 +0000

    did some stuffnasdflhajs;ldfkj validations and break stuff

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -64,7 +64,7 @@ class EventsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event
-      @event = Event.find(params[:id])
+      @event = Event.find_by_id(params[:id])
       @comments = @event.comments.all
       @comment = @event.comments.build
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :comments
+  
   devise_for :users
   get 'pages/info'
-
+  
+  resources :comments
   resources :interests
   resources :ideas
   resources :events
   get 'welcome/index'
-  get "pages/info"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
