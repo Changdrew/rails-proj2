58995e8a0cba0f4de4295878f478d4467cafebd2
Kevin Shi <kshi@berkeley.edu>
2015-12-02 19:55:08 -0800

    fixed events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
 	geocoded_by :address
 	after_validation :geocode, :if => :address_changed?
-    validate :address, presence: true
+    validates :address, presence: true
     has_many :comments
 end
