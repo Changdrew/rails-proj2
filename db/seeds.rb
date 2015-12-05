23ba37b0fca9814a74372e278e6c3dfa328842e0
Kevin Shi <kshi@berkeley.edu>
2015-12-04 23:34:50 -0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  Interest.create(name: 'Photography', description: 'Come share our love for photography!', picture: 'photo.jpeg')
